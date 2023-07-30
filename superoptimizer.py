7015a2d8fbaf851ba621bbed348ffd255035bbd2
Dong-hee Na <donghee.na@python.org>
2023-07-31 01:06:12 +0900

    Run black

--- superoptimizer.py
+++ superoptimizer.py
@@ -52,11 +52,19 @@ class VirtualInstruction:
         return self.__str__()
 
     def __str__(self) -> str:
-        return f'({self.opname}, {self.arg})'
+        return f"({self.opname}, {self.arg})"
 
     @staticmethod
     def ops():
-        return ("LOAD_CONST", "UNPACK_SEQUENCE", "STORE_FAST", "LOAD_FAST", "SWAP", "POP_TOP", "RETURN_VALUE")
+        return (
+            "LOAD_CONST",
+            "UNPACK_SEQUENCE",
+            "STORE_FAST",
+            "LOAD_FAST",
+            "SWAP",
+            "POP_TOP",
+            "RETURN_VALUE",
+        )
 
 
 class Program:
@@ -67,8 +75,8 @@ class Program:
                 # Filter out RESUME
                 continue
             self.instructions.append(inst)
-        self.co_consts = co_consts # [c for c in f.__code__.co_consts]
-        self.co_varnames = co_varnames # [c for c in f.__code__.co_varnames]
+        self.co_consts = co_consts  # [c for c in f.__code__.co_consts]
+        self.co_varnames = co_varnames  # [c for c in f.__code__.co_varnames]
 
     @classmethod
     def from_function(cls, f):
@@ -79,7 +87,11 @@ class Program:
                 continue
             instruction = VirtualInstruction(inst.opname, inst.arg)
             instructions.append(instruction)
-        return cls(instructions, copy.deepcopy(f.__code__.co_consts), copy.deepcopy(f.__code__.co_varnames))
+        return cls(
+            instructions,
+            copy.deepcopy(f.__code__.co_consts),
+            copy.deepcopy(f.__code__.co_varnames),
+        )
 
     def cost(self) -> int:
         ret = 0
@@ -111,67 +123,89 @@ class VM:
             inst = self.intructions[self.pc]
             match inst.opname:
                 case "RESUME":
-                    self.pc+=1
+                    self.pc += 1
                 case "LOAD_CONST":
                     # https://docs.python.org/3.11/library/dis.html#opcode-LOAD_CONST
                     consti = inst.arg
                     self.stack.append(self.co_consts[consti])
-                    self.pc+=1
+                    self.pc += 1
                 case "UNPACK_SEQUENCE":
                     # https://docs.python.org/3.13/library/dis.html#opcode-UNPACK_SEQUENCE
                     count = inst.arg
-                    assert(len(self.stack[-1]) == count)
-                    self.stack.extend(self.stack.pop()[:-count-1:-1])
-                    self.pc+=1
+                    assert len(self.stack[-1]) == count
+                    self.stack.extend(self.stack.pop()[: -count - 1 : -1])
+                    self.pc += 1
                 case "STORE_FAST":
                     # https://docs.python.org/3.11/library/dis.html#opcode-STORE_FAST
                     var_num = inst.arg
                     top = self.stack.pop()
                     self.co_varnames[var_num] = top
-                    self.pc+=1
+                    self.pc += 1
                 case "LOAD_FAST":
                     # https://docs.python.org/3.11/library/dis.html#opcode-LOAD_FAST
                     var_num = inst.arg
                     self.stack.append(self.co_varnames[var_num])
-                    self.pc+=1
+                    self.pc += 1
                 case "SWAP":
                     # https://docs.python.org/3.11/library/dis.html#opcode-SWAP
                     i = inst.arg
                     self.stack[-i], self.stack[-1] = self.stack[-1], self.stack[-i]
-                    self.pc+=1
+                    self.pc += 1
                 case "POP_TOP":
                     self.stack.pop()
-                    self.pc+=1
+                    self.pc += 1
                 case "RETURN_VALUE":
                     self.ret = self.stack[-1]
                     break
                 case _:
-                    raise RuntimeError(f'Unsupported opcodes: {inst.opname}')
-        memory_state = MemoryState(self.stack, self.co_consts, self.co_varnames, self.ret)
+                    raise RuntimeError(f"Unsupported opcodes: {inst.opname}")
+        memory_state = MemoryState(
+            self.stack, self.co_consts, self.co_varnames, self.ret
+        )
         return memory_state
 
 
 class Superoptimizer:
-
     def __init__(self, program: Program):
         self.program = program
 
     def generate_programs(self) -> Generator[Program, None, None]:
         for length in range(1, len(self.program) + 1):
-            for instructions in itertools.product(VirtualInstruction.ops(), repeat=length):
+            for instructions in itertools.product(
+                VirtualInstruction.ops(), repeat=length
+            ):
                 arg_sets = []
                 for inst in instructions:
                     match inst:
                         case "LOAD_CONST":
-                            arg_sets.append([tuple([val]) for val in range(len(self.program.co_consts))])
+                            arg_sets.append(
+                                [
+                                    tuple([val])
+                                    for val in range(len(self.program.co_consts))
+                                ]
+                            )
                         case "UNPACK_SEQUENCE":
-                            arg_sets.append([tuple([val]) for val in range(len(instructions))])
+                            arg_sets.append(
+                                [tuple([val]) for val in range(len(instructions))]
+                            )
                         case "STORE_FAST":
-                            arg_sets.append([tuple([val]) for val in range(len(self.program.co_varnames))])
+                            arg_sets.append(
+                                [
+                                    tuple([val])
+                                    for val in range(len(self.program.co_varnames))
+                                ]
+                            )
                         case "LOAD_FAST":
-                            arg_sets.append([tuple([val]) for val in range(len(self.program.co_varnames))])
+                            arg_sets.append(
+                                [
+                                    tuple([val])
+                                    for val in range(len(self.program.co_varnames))
+                                ]
+                            )
                         case "SWAP":
-                            arg_sets.append([tuple([val]) for val in range(len(instructions))])
+                            arg_sets.append(
+                                [tuple([val]) for val in range(len(instructions))]
+                            )
                         case "POP_TOP":
                             arg_sets.append([(None,)])
                         case "RETURN_VALUE":
@@ -183,9 +217,11 @@ class Superoptimizer:
                         virtual_instruction = VirtualInstruction(inst, *args)
                         virtual_instructions.append(virtual_instruction)
 
-                    generated_program = Program(virtual_instructions,
-                                                copy.deepcopy(self.program.co_consts),
-                                                copy.deepcopy(self.program.co_varnames))
+                    generated_program = Program(
+                        virtual_instructions,
+                        copy.deepcopy(self.program.co_consts),
+                        copy.deepcopy(self.program.co_varnames),
+                    )
                     yield generated_program
 
     def search(self) -> Program:
@@ -193,28 +229,35 @@ class Superoptimizer:
         origin_state = origin_vm.run()
         print("========================================================")
         print(f"Original State: {origin_state} / Program: {self.program}")
-        optimized_prog, optimized_state  = self.program, None
+        optimized_prog, optimized_state = self.program, None
         for prog in tqdm.tqdm(self.generate_programs()):
             if len(optimized_prog) < len(prog):
                 break
             vm = VM(prog)
             try:
                 possible_state = vm.run()
-                if possible_state == origin_state and len(prog) <= len(optimized_prog) and prog.cost() < optimized_prog.cost():
+                if (
+                    possible_state == origin_state
+                    and len(prog) <= len(optimized_prog)
+                    and prog.cost() < optimized_prog.cost()
+                ):
                     optimized_prog, optimized_state = prog, possible_state
                     break
             except Exception:
                 continue
 
         if optimized_state is not None:
-            print(f'Found -> Optimized State: {optimized_state}, / Program: {optimized_prog}')
+            print(
+                f"Found -> Optimized State: {optimized_state}, / Program: {optimized_prog}"
+            )
         else:
-            print('Not found!')
+            print("Not found!")
         print("========================================================")
         return optimized_prog
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     def f():
         a, b = 3, 5
         a, b = b, a
